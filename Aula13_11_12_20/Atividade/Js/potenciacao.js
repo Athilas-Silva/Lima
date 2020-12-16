b7f705efbc8737105475c810cea0afc6e1b95204
Athilas-Silva <71888055+Athilas-Silva@users.noreply.github.com>
2020-12-16 15:52:15 -0300

    Add files via upload

---
+++ Aula13_11_12_20/Atividade/Js/potenciacao.js
@@ -0,0 +1,10 @@
+  
+function potenciacao() {
+    let denominador = parseInt(document.getElementById("denominador").value)
+    let expoente = parseInt(document.getElementById("expoente").value)
+    let resultado
+
+    resultado = Math.pow(denominador, expoente);
+
+    document.getElementById("res_potenciacao").textContent = resultado
+}
